723482afcdba08ff96096fb5b73fb49b53e1ed6a
‘leedhfsd’ <leedhfsd@gmail.com>
2024-05-15 21:29:05 +0900

    planning

--- src/views/PlanningView.vue
+++ src/views/PlanningView.vue
@@ -1,27 +1,11 @@
-<script setup></script>
+<script setup>
+import { usePlanningStore } from "@/stores/planning";
+const store = usePlanningStore();
+const plans = store.plans;
+</script>
 
-<template>
-	<v-container>
-		<v-row>
-			<v-col cols="2">
-				<v-sheet rounded="lg">
-					<v-list rounded="lg">
-						<v-list-item v-for="n in 5" :key="n" :title="`List Item ${n}`" link></v-list-item>
+// 일정 생성페이지
 
-						<v-divider class="my-2"></v-divider>
-
-						<v-list-item color="grey-lighten-4" title="Refresh" link></v-list-item>
-					</v-list>
-				</v-sheet>
-			</v-col>
-
-			<v-col>
-				<v-sheet min-height="70vh" rounded="lg">
-					<!--  -->
-				</v-sheet>
-			</v-col>
-		</v-row>
-	</v-container>
-</template>
+<template></template>
 
 <style scoped></style>
