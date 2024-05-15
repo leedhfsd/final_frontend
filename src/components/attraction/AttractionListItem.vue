17aee57783b5a500082a1e2802970904404b175f
leedhfsd <leedhfsd@gmail.com>
2024-05-16 08:47:46 +0900

    revise

--- src/components/attraction/AttractionListItem.vue
+++ src/components/attraction/AttractionListItem.vue
@@ -1,75 +1,75 @@
 <script setup>
-import { defineProps } from "vue";
-import { mdiHeart, mdiStarOutline, mdiStar, mdiStarHalfFull } from "@mdi/js";
+	import { defineProps } from "vue";
+	import { mdiHeart, mdiStarOutline, mdiStar, mdiStarHalfFull } from "@mdi/js";
 
-const props = defineProps({
-  attraction: {
-    type: Object,
-    required: true,
-    id: {
-      type: Number,
-    },
-    title: {
-      type: String,
-    },
-    description: {
-      type: String,
-    },
-    contentTypeId: {
-      type: Number,
-    },
-    firstImage: {
-      type: String,
-    },
-    addr1: {
-      type: String,
-    },
-    like: {
-      type: String,
-    },
-    rating: {
-      type: String,
-    },
-  },
-});
+	const props = defineProps({
+		attraction: {
+			type: Object,
+			required: true,
+			id: {
+				type: Number,
+			},
+			title: {
+				type: String,
+			},
+			description: {
+				type: String,
+			},
+			contentTypeId: {
+				type: Number,
+			},
+			firstImage: {
+				type: String,
+			},
+			addr1: {
+				type: String,
+			},
+			like: {
+				type: String,
+			},
+			rating: {
+				type: String,
+			},
+		},
+	});
 </script>
 
 <template>
-  <v-card>
-    <v-img :src="props.attraction.firstImage" height="200px">
-      <v-btn icon small class="position-absolute top-0 right-0">
-        <v-icon :icon="mdiHeart"></v-icon>
-      </v-btn>
-    </v-img>
-    <v-card-title>{{ props.attraction.title }}</v-card-title>
-    <v-card-subtitle class="pb-0">
-      {{ props.attraction.description }}
-    </v-card-subtitle>
-    <v-card-text class="d-flex align-center">
-      <v-rating
-        :value="parseFloat(props.attraction.rating)"
-        :empty-icon="mdiStarOutline"
-        :full-icon="mdiStar"
-        :half-icon="mdiStarHalfFull"
-        color="primary"
-        density="compact"
-        size="15"
-        half-increments
-        readonly
-      ></v-rating>
-      <span class="text-body-2 ml-2">{{ props.attraction.rating }}</span>
-    </v-card-text>
-    <v-card-text>
-      {{ props.attraction.addr1 }}
-    </v-card-text>
-  </v-card>
+	<v-card>
+		<v-img :src="props.attraction.firstImage" height="200px">
+			<v-btn icon small class="position-absolute top-0 right-0">
+				<v-icon :icon="mdiHeart"></v-icon>
+			</v-btn>
+		</v-img>
+		<v-card-title>{{ props.attraction.title }}</v-card-title>
+		<v-card-subtitle class="pb-0">
+			{{ props.attraction.description }}
+		</v-card-subtitle>
+		<v-card-text class="d-flex align-center">
+			<v-rating
+				:value="parseFloat(props.attraction.rating)"
+				:empty-icon="mdiStarOutline"
+				:full-icon="mdiStar"
+				:half-icon="mdiStarHalfFull"
+				color="primary"
+				density="compact"
+				size="15"
+				half-increments
+				readonly
+			></v-rating>
+			<span class="text-body-2 ml-2">{{ props.attraction.rating }}</span>
+		</v-card-text>
+		<v-card-text>
+			{{ props.attraction.addr1 }}
+		</v-card-text>
+	</v-card>
 </template>
 
 <style scoped>
-.v-card {
-  position: relative;
-}
-.v-btn {
-  background-color: white;
-}
+	.v-card {
+		position: relative;
+	}
+	.v-btn {
+		background-color: white;
+	}
 </style>
--- src/components/user/UserLoginForm.vue
+++ src/components/user/UserLoginForm.vue
@@ -1,53 +1,45 @@
 <script setup>
-import { ref } from "vue";
-const loginUser = ref({
-  id: null,
-  password: null,
-});
+	import { ref } from "vue";
+	const loginUser = ref({
+		id: null,
+		password: null,
+	});
 
-// const login = async () => {
-// 	await userLogin(loginUser.value);
-// 	let token = sessionStorage.getItem("accessToken");
-// 	console.log(token);
-// 	if (!isLogin.value) {
-// 		getUserInfo(token);
-// 		changeMenuState();
-// 		router.replace("/")
-// 	}
-// }
+	// const login = async () => {
+	// 	await userLogin(loginUser.value);
+	// 	let token = sessionStorage.getItem("accessToken");
+	// 	console.log(token);
+	// 	if (!isLogin.value) {
+	// 		getUserInfo(token);
+	// 		changeMenuState();
+	// 		router.replace("/")
+	// 	}
+	// }
 </script>
 
 <template>
-  <v-container>
-    <v-card class="mx-auto" max-width="344" title="로그인">
-      <v-container>
-        <v-form>
-          <v-text-field
-            :v-model="userInfo.id"
-            color="primary"
-            label="아이디"
-            variant="outlined"
-          ></v-text-field>
-          <v-text-field
-            :v-model="userInfo.password"
-            color="primary"
-            label="비밀번호"
-            placeholder="비밀번호를 입력해주세요.."
-            variant="outlined"
-          ></v-text-field>
-          <v-btn
-            color="primary"
-            size="large"
-            type="submit"
-            variant="elevated"
-            block
-          >
-            Sign In
-          </v-btn>
-        </v-form>
-      </v-container>
-    </v-card>
-  </v-container>
+	<v-container>
+		<v-card class="mx-auto" max-width="344" title="로그인">
+			<v-container>
+				<v-form>
+					<v-text-field
+						:v-model="loginUser.id"
+						color="primary"
+						label="아이디"
+						variant="outlined"
+					></v-text-field>
+					<v-text-field
+						:v-model="loginUser.password"
+						color="primary"
+						label="비밀번호"
+						placeholder="비밀번호를 입력해주세요.."
+						variant="outlined"
+					></v-text-field>
+					<v-btn color="primary" size="large" type="submit" variant="elevated" block> Sign In </v-btn>
+				</v-form>
+			</v-container>
+		</v-card>
+	</v-container>
 </template>
 
 <style scoped></style>
